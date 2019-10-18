341fd41e8afe5b752deb3c4a17965d46a8dbdb05
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-18 12:21:17 -0400

    basic card styling with reactstrap

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { Card, CardText, CardTitle } from "reactstrap";
 
 export default function CharacterCard(props) {
-  console.log(props.info.info);
   return (
-    <>
-      <h2>Name: {props.info.info.name}</h2>
-      <p>Species: {props.info.info.species}</p>
-      <p>Status: {props.info.info.status}</p>
-    </>
+    <div className="card-container">
+      <Card>
+        <CardTitle>Name: {props.info.info.name}</CardTitle>
+        <CardText>Species: {props.info.info.species}</CardText>
+        <CardText>Status: {props.info.info.status}</CardText>
+      </Card>
+    </div>
   );
 }
