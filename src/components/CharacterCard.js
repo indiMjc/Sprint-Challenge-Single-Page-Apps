4d0a0188c6c328fa90260cfa8459ac8ca3d466d2
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-18 11:58:56 -0400

    character cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  console.log(props.info.info);
+  return (
+    <>
+      <h2>Name: {props.info.info.name}</h2>
+      <p>Species: {props.info.info.species}</p>
+      <p>Status: {props.info.info.status}</p>
+    </>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios.get(`https://rickandmortyapi.com/api/character/`).then(res => {
+      console.log(res.data.results);
+      setCharacters(res.data.results);
+    });
+  }, [setCharacters]);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {/* <h2>TODO: `array.map()` over your state here!</h2> */}
+      {characters.map((char, i) => (
+        <Link key={i} to={`/characters/${char.name}`}>
+          <CharacterDetails key={char.name} info={char} />
+          {/* <h2>Name: {char.name}</h2>
+          <p>Species: {char.species}</p>
+          <p>Status: {char.status}</p> */}
+        </Link>
+      ))}
     </section>
   );
+}
+
+function CharacterDetails(char) {
+  return <CharacterCard key={char.id} info={char} />;
 }
