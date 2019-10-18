dacb61c987e96d8d8e3ebb8d74314adb9e1bd555
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-18 12:39:05 -0400

    basic search bar added to characters page

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
@@ -9,24 +10,27 @@ export default function CharacterList() {
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios.get(`https://rickandmortyapi.com/api/character/`).then(res => {
-      console.log(res.data.results);
-      setCharacters(res.data.results);
-    });
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(res => {
+        setCharacters(res.data.results);
+      })
+      .catch(error => {
+        console.error("Server Error", error);
+      });
   }, [setCharacters]);
 
   return (
-    <section className="character-list">
-      {/* <h2>TODO: `array.map()` over your state here!</h2> */}
-      {characters.map((char, i) => (
-        <Link key={i} to={`/characters/${char.name}`}>
-          <CharacterDetails key={char.name} info={char} />
-          {/* <h2>Name: {char.name}</h2>
-          <p>Species: {char.species}</p>
-          <p>Status: {char.status}</p> */}
-        </Link>
-      ))}
-    </section>
+    <>
+      <SearchForm characters={characters} />
+      <section className="character-list">
+        {characters.map((char, i) => (
+          <Link key={i} to={`/characters/${char.name}`}>
+            <CharacterDetails key={char.name} info={char} />
+          </Link>
+        ))}
+      </section>
+    </>
   );
 }
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  // console.log("search props", props.characters);
+
+  useEffect(() => {
+    const results = props.characters.filter(character =>
+      character.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <>
+      <form>
+        <label>Search string:</label>
+        <input
+          id="name"
+          type="text"
+          name="textField"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </form>
+      <div className="results">
+        <ul>
+          {searchResults.map(character => (
+            <li key={character.name}>{character.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
